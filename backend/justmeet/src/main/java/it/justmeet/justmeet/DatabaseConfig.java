86c1a827337228dbbe3b1d04947e29804f64e031
Joellao <joel.sina97@gmail.com>
2019-11-22 15:46:49 +0100

    Url in string

--- backend/justmeet/src/main/java/it/justmeet/justmeet/DatabaseConfig.java
+++ backend/justmeet/src/main/java/it/justmeet/justmeet/DatabaseConfig.java
@@ -5,13 +5,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class DatabaseConfig {
-    @Value("spring.datasource.url")
-    static String dbUrl;
-
     public static Connection getConnection() throws URISyntaxException, SQLException {
+        String dbUrl = "jdbc:postgresql://ec2-46-137-120-243.eu-west-1.compute.amazonaws.com:5432/dctq26iqsf16r7?user=kgapespdyqobhl&password=bb765ee804bc0e6d6241e0f5f955e73376a8442426aa1a8b402ec6ad41bcb3d2&sslmode=require";
         return DriverManager.getConnection(dbUrl);
     }
 }
