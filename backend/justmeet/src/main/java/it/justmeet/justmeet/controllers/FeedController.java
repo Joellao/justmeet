776c4ccdc41a2dc36643faedc67e16a2d37c8210
Joel Sina <joel.sina97@gmail.com>
2020-01-16 17:33:10 +0100

    Modifiche per vedere annunci

--- backend/justmeet/src/main/java/it/justmeet/justmeet/controllers/FeedController.java
+++ backend/justmeet/src/main/java/it/justmeet/justmeet/controllers/FeedController.java
@@ -23,32 +23,31 @@ public class FeedController {
 
 	@Autowired
 	UserRepository userRepo; // jpa è una libreria
-	
+
+	@GetMapping("/getAllEvents")
 	private List<Event> getEvents(@RequestHeader("Authorization") String token) throws FirebaseAuthException {
 		String userId = WoWoUtility.getInstance().getUid(token);
 		AbstractUser user = userRepo.findByUid(userId);
-		List<AbstractUser> friends=user.getFriends();
-		List<Event> events=new ArrayList<Event>();
-		friends.forEach(friend -> 
-			events.addAll(friend.getEvents())
-		);
+		List<AbstractUser> friends = user.getFriends();
+		List<Event> events = new ArrayList<Event>();
+		friends.forEach(friend -> events.addAll(friend.getEvents()));
 		return events;
 	}
-	
-	private List<Announcement> getAnnouncement(@RequestHeader("Authorization") String token) throws FirebaseAuthException {
+
+	@GetMapping("/getAllAnnouncements")
+	private List<Announcement> getAnnouncement(@RequestHeader("Authorization") String token)
+			throws FirebaseAuthException {
 		String userId = WoWoUtility.getInstance().getUid(token);
 		AbstractUser user = userRepo.findByUid(userId);
-		List<User> friends=(List<User>)(List<?>) user.getFriends();
-		List<Announcement> announcement=new ArrayList<Announcement>();
-		friends.forEach(friend -> 
-			announcement.addAll(friend.getAnnouncements())
-		);
+		List<User> friends = (List<User>) (List<?>) user.getFriends();
+		List<Announcement> announcement = new ArrayList<Announcement>();
+		friends.forEach(friend -> announcement.addAll(friend.getAnnouncements()));
 		return announcement;
 	}
-	
+
 	@GetMapping("/feed")
-	public List<Object> getFeed(@RequestHeader("Authorization") String token) throws FirebaseAuthException{
-		List<Object> lista=new ArrayList<Object>();
+	public List<Object> getFeed(@RequestHeader("Authorization") String token) throws FirebaseAuthException {
+		List<Object> lista = new ArrayList<Object>();
 		lista.addAll(getAnnouncement(token));
 		lista.addAll(getEvents(token));
 		return lista;
--- frontend/justmeet/lib/screens/feed_screen.dart
+++ frontend/justmeet/lib/screens/feed_screen.dart
@@ -26,124 +26,124 @@ class _FeedScreenState extends State<FeedScreen> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     robe = getDataEvent();
-      }
-    
-      Future<List<dynamic>> getDataEvent() async {
-        String token = Provider.of<String>(context);
-        Response response = await dio.get(
-          "https://justmeetgjj.herokuapp.com/feed/",
-          options: Options(
-            headers: {
-              "Authorization": token,
-            },
-          ),
-        );  
-      
-        if (response.statusCode == 200) {
-          List<dynamic> robe = [];
-          List robe2 = response.data;
-          print(await Provider.of<User>(context).uid);
-          robe2.forEach((robbo) {
-            if(robbo['location']!= null){
-robe.add(Event.fromJson(robbo));
-            }
-            
-            else {robe.add(Announcement.fromJson(robbo));};
-          });
-      
-          
-          return robe;
+  }
+
+  Future<List<dynamic>> getDataEvent() async {
+    String token = Provider.of<String>(context);
+    Response response = await dio.get(
+      "https://justmeetgjj.herokuapp.com/feed/",
+      options: Options(
+        headers: {
+          "Authorization": token,
+        },
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> robe = [];
+      List robe2 = response.data;
+      robe2.forEach((robbo) {
+        if (robbo['location'] != null) {
+          robe.add(Event.fromJson(robbo));
         } else {
-          return null;
+          robe.add(Announcement.fromJson(robbo));
         }
-      }
-    
-      @override
-      Widget build(BuildContext context) {
-        return Container(
-          color: Colori.bluScuro,
-          child: FutureBuilder<List<dynamic>>(
-            initialData: [],
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    var event = snapshot.data.elementAt(index);
-                    if(event is Event)
-                    return EventWidget(
-                      event: event,
-                      profileWidget: getProfileWidget(event),
-                    );
-                    else return AnnouncementWidget(announcement: event,);
-                  },
-                );
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              return Center(child: CircularProgressIndicator());
-            },
-            future: robe,
-          ),
-        );
-      }
-    
-      Widget getProfileWidget(Event event) {
-        return Container(
-          padding: EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ;
+      });
+
+      return robe;
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colori.bluScuro,
+      child: FutureBuilder<List<dynamic>>(
+        initialData: [],
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                var event = snapshot.data.elementAt(index);
+                if (event is Event)
+                  return EventWidget(
+                    event: event,
+                    profileWidget: getProfileWidget(event),
+                  );
+                else
+                  return AnnouncementWidget(
+                    announcement: event,
+                  );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+        future: robe,
+      ),
+    );
+  }
+
+  Widget getProfileWidget(Event event) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: event.user.profileImage != ""
-                        ? NetworkImage(event.user.profileImage)
-                        : null,
-                    child: event.user.profileImage == ""
-                        ? Icon(Icons.person, size: 25)
-                        : null,
-                  ),
-                  SizedBox(
-                    width: 7.0,
-                  ),
-                  Text(
-                    event.user.firstName,
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.roboto(
-                      textStyle: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(" "),
-                  Text(
-                    event.user.lastName,
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.roboto(
-                      textStyle: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+              CircleAvatar(
+                backgroundImage: event.user.profileImage != ""
+                    ? NetworkImage(event.user.profileImage)
+                    : null,
+                child: event.user.profileImage == ""
+                    ? Icon(Icons.person, size: 25)
+                    : null,
               ),
-              InkWell(
-                onTap: () => print("More premuto"),
-                child: Icon(
-                  Icons.more_vert,
-                  size: 30.0,
+              SizedBox(
+                width: 7.0,
+              ),
+              Text(
+                event.user.firstName,
+                textAlign: TextAlign.left,
+                style: GoogleFonts.roboto(
+                  textStyle: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(" "),
+              Text(
+                event.user.lastName,
+                textAlign: TextAlign.left,
+                style: GoogleFonts.roboto(
+                  textStyle: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                overflow: TextOverflow.ellipsis,
               ),
             ],
           ),
-        );
-      }
-    }
-
-
+          InkWell(
+            onTap: () => print("More premuto"),
+            child: Icon(
+              Icons.more_vert,
+              size: 30.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
