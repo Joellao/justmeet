70273035da17377080bfb92a60ab3bd8cf84c7f7
Giulia Morelli <giulia.morelli@studenti.unicam.it>
2020-02-04 17:05:30 +0100

    aggiunti getter id della recensione

--- backend/justmeet/src/main/java/it/justmeet/justmeet/models/Review.java
+++ backend/justmeet/src/main/java/it/justmeet/justmeet/models/Review.java
@@ -63,7 +63,15 @@ public class Review {
         this.date = date;
     }
 
-    public void setEvent(Event event) {
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setEvent(Event event) {
         this.event = event;
     }
 
